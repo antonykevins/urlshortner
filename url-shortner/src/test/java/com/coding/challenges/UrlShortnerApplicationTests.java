2c7e324e75ce15853dbdfdbd99ec20e43473e360
antonykevins <40645335+antonykevins@users.noreply.github.com>
2021-02-22 10:56:00 +0000

    Add files via upload

---
+++ url-shortner/src/test/java/com/coding/challenges/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
